7c1c70e2d63be5ca73cd7274cb54d902f31632bb
JChase2 <mr.jdchase@gmail.com>
2020-01-30 07:26:24 -0500

    Had to install an older version of react navigation to support this course. Didn't feel like mucking about with newer stuff while there's assignments to be done on this code. Also, added some new nav stuff.

--- componenets/DishDetailComponent.js
+++ componenets/DishDetailComponent.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function RenderDish(props) {
     const dish = props.dish;
-    if (dish != null){
+    if (dish != null) {
         return (
-            <Card 
+            <Card
                 featuredTitle={dish.name}
                 image={require('./images/uthappizza.png')}>
-                <Text style={{margin: 10}}>
+                <Text style={{ margin: 10 }}>
                     {dish.description}
                 </Text>
             </Card>
@@ -20,10 +21,27 @@ function RenderDish(props) {
     }
 }
 
-function Dishdetail(props) {
-    return(
-        <RenderDish dish={props.dish} />
-    );
+class Dishdetail extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    };
+
+    static navigationOptions = {
+        title: 'Dish Details',
+
+
+    }
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId', '');
+        return (
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        );
+    }
 }
 
 export default Dishdetail;
