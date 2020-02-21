835ae11c76f3d84402e2228ff1585279e58db1e5
JChase2 <mr.jdchase@gmail.com>
2020-02-21 01:18:37 -0500

    Added email function to contact component to automatically open up and fill in your phones email app.

--- componenets/ContactComponent.js
+++ componenets/ContactComponent.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { Card } from 'react-native-elements';
-import { Text } from 'react-native';
+import { Text, Button, Icon } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
+
+    sendMail(){
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern, '
+        })
+    }
+
     render() {
         return (
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -15,6 +25,12 @@ class Contact extends Component {
                     <Text style={{ marginBottom: 10 }}>Tel: +852 1234 5678</Text>
                     <Text style={{ marginBottom: 10 }}>Fax: +852 8765 4321</Text>
                     <Text style={{ marginBottom: 10 }}>Email:confusion@food.net</Text>
+                    <Button 
+                        title='Send Email'
+                        buttonStyle={{backgroundColor: '#512DA8'}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white'/>}
+                        onPress={() => this.sendMail()}
+                    />
                 </Card>
             </Animatable.View>
         );
